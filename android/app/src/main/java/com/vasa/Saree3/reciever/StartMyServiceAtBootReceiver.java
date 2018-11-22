26fbd9aa1890ca5614444850a391270fe2de1fd4
man-r <man-r@github.com>
2018-11-22 14:27:23 +0300

    bring back the normal gps trakers not google one

--- android/app/src/main/java/com/vasa/Saree3/reciever/StartMyServiceAtBootReceiver.java
+++ android/app/src/main/java/com/vasa/Saree3/reciever/StartMyServiceAtBootReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.vasa.Saree3.Constants;
+import com.vasa.Saree3.MyService;
 import com.vasa.Saree3.MyService2;
 
 public class StartMyServiceAtBootReceiver extends BroadcastReceiver {
@@ -12,7 +13,7 @@ public class StartMyServiceAtBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent startIntent = new Intent(context, MyService2.class);
+            Intent startIntent = new Intent(context, MyService.class);
 	  		startIntent.setAction(Constants.ACTION.STARTFOREGROUND_ACTION);
 			context.startService(startIntent);
         }
