9c376d04a6af03d7c873710aabba1fd518033a38
man-r <man-r@github.com>
2018-11-29 15:41:32 +0300

    geoViewActivity

---
+++ android/app/src/main/java/com/vasa/Saree3/GeoViewActivity.java
@@ -0,0 +1,12 @@
+package com.vasa.Saree3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeoViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
